afcc2b8038c8724bb7a842faa64ee6711cbfdc6f
Sreejith Viswambharan <sreejith.v@pfizer.com>
2023-02-22 18:07:20 +0530

    Initial commit

---
+++ android/src/main/java/com/pfizer/unzip/Zip.java
@@ -0,0 +1,11 @@
+package com.pfizer.unzip;
+
+import android.util.Log;
+
+public class Zip {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
